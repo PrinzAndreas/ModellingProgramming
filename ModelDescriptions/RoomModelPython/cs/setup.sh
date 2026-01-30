a0d5a1aef4979bb0aeecd8e0b64953b728612582
devabz <devabzko@gmail.com>
2026-01-30 04:14:33 +0100

    updated devcontainer

--- ModelDescriptions/RoomModelPython/cs/setup.sh
+++ ModelDescriptions/RoomModelPython/cs/setup.sh
@@ -1,10 +1,10 @@
 apt update 
-apt install -y python3.6 python3.6-dev python3-pip gnuplot 
-update-alternatives --install /usr/bin/python python /usr/bin/python3.6 1
+apt install -y gnuplot 
+# update-alternatives --install /usr/bin/python python /usr/bin/python3.6 1
 
 
 # INSTALL REQUIREMENTS
-pip install -r cs/requirements.txt
+# pip install -r cs/requirements.txt
 
 # # REMOVE EXCESS PYTHON INTERPRETERS 
 # rm -rf $(find /usr/bin -type f -name python*)
