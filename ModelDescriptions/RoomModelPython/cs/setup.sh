e037a182cedc230d4f297700c069618b0a071d38
devabz <devabzko@gmail.com>
2026-01-30 04:47:10 +0100

    updated devcontainer

--- ModelDescriptions/RoomModelPython/cs/setup.sh
+++ ModelDescriptions/RoomModelPython/cs/setup.sh
@@ -4,6 +4,9 @@ export DEBIAN_FRONTEND=noninteractive
 cp /etc/apt/sources.list /etc/apt/sources.list.backup
 
 # Update sources.list to use archive.debian.org for old releases
+sed -i 's/deb.debian.org/archive.debian.org/g' /etc/apt/sources.list
+sed -i 's|security.debian.org|archive.debian.org/debian-security|g' /etc/apt/sources.list
+sed -i '/buster-updates/d' /etc/apt/sources.list
 sed -i 's|archive.debian.org/debian-security/debian-security|archive.debian.org/debian-security|g' /etc/apt/sources.list
 
 # Update package lists
